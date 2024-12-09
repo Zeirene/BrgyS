df2501052306639d355374925d8a9137d8d39278
Magnaye16 <151270874+Magnaye16@users.noreply.github.com>
2024-12-10 04:06:56 +0800

    fix

--- BrgyS/Form6.Designer.vb
+++ BrgyS/Form6.Designer.vb
@@ -27,16 +27,18 @@ Partial Class Form6
         Dim DataGridViewCellStyle3 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Guna2DataGridView1 = New Guna.UI2.WinForms.Guna2DataGridView()
         Names = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
         Column2 = New DataGridViewTextBoxColumn()
         Column3 = New DataGridViewTextBoxColumn()
         Column5 = New DataGridViewTextBoxColumn()
-        Column6 = New DataGridViewTextBoxColumn()
         Column1 = New DataGridViewTextBoxColumn()
         CType(Guna2DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Guna2DataGridView1
         ' 
+        Guna2DataGridView1.AllowUserToAddRows = False
+        Guna2DataGridView1.AllowUserToDeleteRows = False
         DataGridViewCellStyle1.BackColor = Color.FromArgb(CByte(44), CByte(48), CByte(52))
         Guna2DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
         Guna2DataGridView1.BorderStyle = BorderStyle.FixedSingle
@@ -50,7 +52,7 @@ Partial Class Form6
         Guna2DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
         Guna2DataGridView1.ColumnHeadersHeight = 17
         Guna2DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing
-        Guna2DataGridView1.Columns.AddRange(New DataGridViewColumn() {Names, Column2, Column3, Column5, Column6, Column1})
+        Guna2DataGridView1.Columns.AddRange(New DataGridViewColumn() {Names, Column4, Column2, Column3, Column5, Column1})
         DataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle3.BackColor = Color.FromArgb(CByte(33), CByte(37), CByte(41))
         DataGridViewCellStyle3.Font = New Font("Segoe UI", 9F)
@@ -62,6 +64,7 @@ Partial Class Form6
         Guna2DataGridView1.GridColor = Color.FromArgb(CByte(50), CByte(56), CByte(62))
         Guna2DataGridView1.Location = New Point(3, 12)
         Guna2DataGridView1.Name = "Guna2DataGridView1"
+        Guna2DataGridView1.ReadOnly = True
         Guna2DataGridView1.RowHeadersVisible = False
         Guna2DataGridView1.Size = New Size(836, 583)
         Guna2DataGridView1.TabIndex = 12
@@ -79,7 +82,7 @@ Partial Class Form6
         Guna2DataGridView1.ThemeStyle.HeaderStyle.ForeColor = Color.White
         Guna2DataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing
         Guna2DataGridView1.ThemeStyle.HeaderStyle.Height = 17
-        Guna2DataGridView1.ThemeStyle.ReadOnly = False
+        Guna2DataGridView1.ThemeStyle.ReadOnly = True
         Guna2DataGridView1.ThemeStyle.RowsStyle.BackColor = Color.FromArgb(CByte(33), CByte(37), CByte(41))
         Guna2DataGridView1.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal
         Guna2DataGridView1.ThemeStyle.RowsStyle.Font = New Font("Segoe UI", 9F)
@@ -90,33 +93,39 @@ Partial Class Form6
         ' 
         ' Names
         ' 
-        Names.HeaderText = "Resident Name"
+        Names.HeaderText = "Resident ID"
         Names.Name = "Names"
+        Names.ReadOnly = True
+        ' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Resident Name"
+        Column4.Name = "Column4"
+        Column4.ReadOnly = True
         ' 
         ' Column2
         ' 
-        Column2.HeaderText = "Staff Name"
+        Column2.HeaderText = "Permit Log ID"
         Column2.Name = "Column2"
+        Column2.ReadOnly = True
         ' 
         ' Column3
         ' 
-        Column3.HeaderText = "Date Time"
+        Column3.HeaderText = "Business Name"
         Column3.Name = "Column3"
+        Column3.ReadOnly = True
         ' 
         ' Column5
         ' 
-        Column5.HeaderText = "Payment"
+        Column5.HeaderText = "Business Address"
         Column5.Name = "Column5"
-        ' 
-        ' Column6
-        ' 
-        Column6.HeaderText = "Document Type"
-        Column6.Name = "Column6"
+        Column5.ReadOnly = True
         ' 
         ' Column1
         ' 
         Column1.HeaderText = "Status"
         Column1.Name = "Column1"
+        Column1.ReadOnly = True
         ' 
         ' Form6
         ' 
@@ -139,9 +148,9 @@ Partial Class Form6
 
     Friend WithEvents Guna2DataGridView1 As Guna.UI2.WinForms.Guna2DataGridView
     Friend WithEvents Names As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
     Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Column5 As DataGridViewTextBoxColumn
-    Friend WithEvents Column6 As DataGridViewTextBoxColumn
     Friend WithEvents Column1 As DataGridViewTextBoxColumn
 End Class
